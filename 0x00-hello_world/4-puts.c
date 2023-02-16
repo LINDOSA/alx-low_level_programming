410baf42bb54436f31d1018097932b23699546b5
LINDOSA <detelepathsa@gmail.com>
2023-02-16 15:28:31 -0800

    0x00-hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 include <stdio.h>
-/*
- * main - print "Programming is like building a multilingual puzzle"
-return (0);
+/**
+ * main - main block
+*return : 0
 */
-
 int main(void)
-
 {    puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
