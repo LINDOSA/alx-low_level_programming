97cbd2a27ff560b0d292e544d8d422da92763ec0
LINDOSA <detelepathsa@gmail.com>
2023-02-16 14:45:49 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ * main - prints ""Programming is like building a multilingual puzzle"
+return (0);
+*/
+
 int main(void)
 {    put("\""Programming is like building a multilingual puzzle");
     return (0);
-}
