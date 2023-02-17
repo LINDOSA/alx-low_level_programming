f393c972814e81ca91a28637121128937b6ab2f0
LINDOSA <detelepathsa@gmail.com>
2023-02-17 00:49:55 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a line of code using put
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        puts("\"with proper grammar, but the outcome is a piece of art");
+        return (0);
+}
