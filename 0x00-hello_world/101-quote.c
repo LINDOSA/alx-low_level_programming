aba52dfd0e21b2da6e7a0fba25b5464ac89b619e
LINDOSA <detelepathsa@gmail.com>
2023-02-17 13:53:07 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistdio.h>
 /**
- * main - Entry point
+ * main - A C program that prints aline to the standard error
  * Return: 1
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	put("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
