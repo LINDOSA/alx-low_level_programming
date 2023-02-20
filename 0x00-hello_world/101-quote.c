e8736fbc3b2db3d5180524d685f6d8c2b5b9eae6
LINDOSA <detelepathsa@gmail.com>
2023-02-20 02:58:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - A C program that prints aline to the standard error
+/** main - print "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to print standard error
+ *
  * Return: 1
  */
 int main(void)
 {
-	put("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
