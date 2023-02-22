dada45ad09e32eb3f18294de424984f5863cfe5c
LINDOSA <detelepathsa@gmail.com>
2023-02-22 05:14:21 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - function that print 10X alphabets
+ * main - check the code.
  * return always 0
 */
 
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	for (i = 1 ; i <= 10 ; i++)
 	{
-		for (j = 'a' ; j <= z ; j++)
+		for (j = 'a' ; j <= 'z' ; j++)
 			_putchar(j);
 	_putchar('\n');
 	}
